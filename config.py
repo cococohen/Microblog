eac82df154f89aba73af004e24e025b4461e337d
Jake Cohen <cococohen1122@gmail.com>
2016-06-28 21:10:47 -0400

    First commit

---
+++ config.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+
+# mail server settings
+MAIL_SERVER = 'plus.smtp.yahooemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+# administrator list
+ADMINS = ['jacob.cohen327@yahoo.com']
+
+# pagination
+POSTS_PER_PAGE = 3
+MAX_SEARCH_RESULTS = 50
+
+#Languages
+LANGUAGES = {
+	'en': 'Engligh',
+	'es': 'Espanñol'
+}
